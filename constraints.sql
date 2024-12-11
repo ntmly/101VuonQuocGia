79e676089acc4371392920bd045788b9b849dd63
Thanh Huyền <90076415+abcdSayHi1907@users.noreply.github.com>
2024-12-11 20:50:52 +0700

    Create constraints.sql
    upload constraints.sql

---
+++ constraints.sql
@@ -0,0 +1,40 @@
+USE vuonquocgia;
+-- FK Animals
+ALTER TABLE Animals
+ADD CONSTRAINT fk_animals
+FOREIGN KEY (HabitatID)
+REFERENCES Habitats(HabitatID);
+
+-- FK AnimalLocations
+ALTER TABLE AnimalLocations
+ADD CONSTRAINT fk_animalLocation_1 FOREIGN KEY (AnimalID) 
+	REFERENCES Animals(AnimalID),
+ADD CONSTRAINT fk_animalLocation_2 FOREIGN KEY (ZoneID) 
+	REFERENCES ParkZones(ZoneID);
+
+-- FK Staff
+ALTER TABLE Staff
+ADD CONSTRAINT fk_staff
+FOREIGN KEY (ZoneID)
+REFERENCES ParkZones(ZoneID);
+
+-- FK Reasearch
+ALTER TABLE Research
+ADD CONSTRAINT fk_research_1 FOREIGN KEY (AnimalID) 
+	REFERENCES Animals(AnimalID),
+ADD CONSTRAINT fk_Research_2 FOREIGN KEY (StaffID) 
+	REFERENCES Staff(StaffID);
+    
+-- FK Tourists
+ALTER TABLE Tourists
+ADD CONSTRAINT fk_tourists 
+FOREIGN KEY (ZoneID)
+REFERENCES ParkZones(ZoneID);
+
+-- FK Events
+ALTER TABLE Events
+ADD CONSTRAINT fk_events
+FOREIGN KEY (ZoneID)
+REFERENCES ParkZones(ZoneID);
+
+
